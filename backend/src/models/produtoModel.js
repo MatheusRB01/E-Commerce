e9653bbb0575c0f86b9500c314559d79e6cd8415
MatheusRB01 <bonfimromanomatheus@gmail.com>
2026-05-18 12:03:39 -0300

    testes

--- backend/src/models/produtoModel.js
+++ backend/src/models/produtoModel.js
@@ -1,12 +1,26 @@
 import db from '../config/db.js'
 
-export default (sequelize, DataTypes) => {
-  const Produto = sequelize.define("Produto", {
-    nome: DataTypes.STRING,
-    preco: DataTypes.FLOAT,
-    descricao: DataTypes.TEXT,
-    imagem: DataTypes.STRING
-  })
+export const getAll = async () => {
+  const [rows] = await db.query('SELECT * FROM produtos')
+  return rows
+}
 
-  return Produto
+export const getById = async (id) => {
+  const [rows] = await db.query('SELECT * FROM produtos WHERE id = ?', [id])
+  return rows[0]
+}
+
+export const create = async (produto) => {
+  const [result] = await db.query('INSERT INTO produtos SET ?', produto)
+  return result
+}
+
+export const update = async (id, produto) => {
+  const [result] = await db.query('UPDATE produtos SET ? WHERE id = ?', [produto, id])
+  return result
+}
+
+export const remove = async (id) => {
+  const [result] = await db.query('DELETE FROM produtos WHERE id = ?', [id])
+  return result
 }
