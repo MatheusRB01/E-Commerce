5ec174b48cea5c88125ea047560ecad03735aa6c
MatheusRB01 <bonfimromanomatheus@gmail.com>
2026-05-18 21:53:51 -0300

    testes

--- frontend/js/usuarios-admin.js
+++ frontend/js/usuarios-admin.js
@@ -3,7 +3,7 @@ async function carregarUsuarios() {
     try {
 
         const response = await fetch(
-            'https://e-commerce-production-b6bf.up.railway.app/admin/usuarios'
+            'https://e-commerce-production-4f36.up.railway.app/admin/usuarios'
         )
 
 
@@ -60,7 +60,7 @@ async function excluirUsuario(id) {
 
         const response = await fetch(
 
-            `https://e-commerce-production-b6bf.up.railway.app/admin/usuarios/${id}`,
+            `https://e-commerce-production-4f36.up.railway.app/admin/usuarios/${id}`,
 
             {
                 method: 'DELETE'
